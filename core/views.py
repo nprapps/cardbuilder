27300cbf5f5a9ef9c7319c665b887aaf439ade08
Tyler Fisher <tyler@tylerjfisher.com>
2017-04-13 14:31:51 -0400

    scaffold first view

--- core/views.py
+++ core/views.py
@@ -1,6 +1,27 @@
+import app_config 
+
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Card, Category
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello, world. This is the index.')
+    context = build_context()
+    context['grouped_cards'] = {}
+    categories = Category.objects.all()
+
+    for category in categories:
+        cards = Card.objects.filter(category=category)
+        context['grouped_cards'][category] = cards
+
+    return render(request, 'index.html', context)
+
+
+def build_context():
+    config = {}
+
+    # Only all-caps [constant] vars get included
+    for k, v in app_config.__dict__.items():
+        if k.upper() == k:
+            config[k] = v
+
+    return config
