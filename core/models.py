8828c89c3d0357fa5dfe9ba3672ff1955b2ea596
Tyler Fisher <tyler@tylerjfisher.com>
2017-04-13 11:06:03 -0400

    scaffold models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Author(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    author_title = models.CharField(max_length=100)
+    author_image = models.URLField(null=True, blank=True)
+    author_page = models.URLField(null=True, blank=True)
+
+    def __str__(self):
+        return '{0} {1}'.format(self.first_name, self.last_name)
+
+
+class Card(models.Model):
+    published = models.BooleanField(default=False)
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+    image = models.URLField(null=True, blank=True)
+    authors = models.ManyToManyField(Author)
+    category = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.title
