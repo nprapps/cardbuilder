194bb44eee992d246ea6f13353da7c84c80207eb
Tyler Fisher <tyler@tylerjfisher.com>
2017-04-18 15:24:43 -0400

    designing the card builder

--- assets/js/app.js
+++ assets/js/app.js
@@ -66,7 +66,8 @@ class App extends React.Component {
             <div>
                 <Categories update={this.updateSelections} key="Categories" />
                 <SelectionList selections={this.state.selections} update={this.updateOrder} key="SelectionList" />
-                <Embed selections={this.state.selections} ids={this.state.ids} key="Embed" />
+                
+                { this.state.selections.length > 0 ? <Embed selections={this.state.selections} ids={this.state.ids} key="Embed" /> : null }
             </div>
         )
     }
@@ -83,15 +84,20 @@ class Categories extends React.Component {
 
     render() {
         return(
-            <div className="categories">
-                {Object.keys(DATA).map(key => (
-                    <CardList 
-                        update={this.update}
-                        category={key} 
-                        cards={DATA[key]}
-                        key={key}
-                    />
-                ))}
+            <div className="row categories-wrapper">
+                <div className="row-label">
+                    <h1>1. Select your cards</h1>
+                </div>
+                <div className="categories row-interaction">
+                    {Object.keys(DATA).map(key => (
+                        <CardList 
+                            update={this.update}
+                            category={key} 
+                            cards={DATA[key]}
+                            key={key}
+                        />
+                    ))}
+                </div>
             </div>
         )
     }
@@ -109,7 +115,7 @@ class CardList extends React.Component {
     render() {
         return(
             <div className="category">
-                <h1>{this.props.category}</h1>
+                <h2>{this.props.category}</h2>
                 {this.props.cards.map((card) => (
                     <Card update={this.updateCard} key={card.id} card={card} />
                 ))}
@@ -135,16 +141,19 @@ class Card extends React.Component {
     render() {
         return(
             <div className="card" id={this.props.card.id}>
-                <label>
-                    <input 
-                        name={this.props.card.title} 
-                        type="checkbox" 
-                        checked={this.state.checked}
-                        onClick={this.toggle}
-                    />
-                    {this.props.card.title}&nbsp;
-                    <a href={`/admin/core/card/${this.props.card.id}/change`}>(edit)</a>
-                </label>
+                <p>
+                        <input 
+                            id={this.props.card.title}
+                            name={this.props.card.title} 
+                            type="checkbox" 
+                            checked={this.state.checked}
+                            onClick={this.toggle}
+                        />
+                        <label htmlFor={this.props.card.title}>
+                        {this.props.card.title}&nbsp;
+                        <a target="_blank" href={`/admin/core/card/${this.props.card.id}/change`}>edit</a>
+                        </label>
+                </p>
             </div>
         )
     }
@@ -160,23 +169,36 @@ class SelectionList extends React.Component {
         this.props.update(updated)
     };
     render() {
-        return <SortableList selections={this.props.selections} onSortEnd={this.onSortEnd} />;
+        return (
+            <div className="row selection-wrapper">
+                <div className="row-label">
+                    <h1>2. Order your cards</h1>
+                </div>
+                <div className="row-interaction selection-list">
+                    <SortableList selections={this.props.selections} onSortEnd={this.onSortEnd} lockAxis="y" helperClass="sorting"/>
+                </div>
+            </div>
+        )
     }
 }
 
 
-const SortableItem = SortableElement(({value}) =>
+const SortableItem = SortableElement(({value}) => 
     <li>{value.title}</li>
 );
 
 const SortableList = SortableContainer(({selections}) => {
   return (
     <div className="selections">
-        <ul>
-          {selections.map((value, index) => (
-            <SortableItem key={`item-${index}`} index={index} value={value} />
-          ))}
-        </ul>
+        { selections.length > 0 ? 
+            <ol>
+              {selections.map((value, index) => (
+                <SortableItem key={`item-${index}`} index={index} value={value} />
+              ))}
+            </ol> :
+            'Select a card!'
+        }
+        
     </div>
   );
 });
@@ -186,7 +208,15 @@ class Embed extends React.Component {
         super(props)
     }
 
+    componentDidMount() {
+        this.initEmbed()
+    }
+
     componentDidUpdate() {
+        this.initEmbed()
+    }
+
+    initEmbed() {
         new pym.Parent(
             'card-embed', 
             `https://s3.amazonaws.com/stage-apps.npr.org/dailygraphics/graphics/trump-card-wireframe-20170410/child.html?ids=${this.props.ids}`, 
@@ -196,7 +226,14 @@ class Embed extends React.Component {
 
     render() {        
         return (
-            <div id="card-embed"></div>
+            <div className="row embed-wrapper">
+                <div className="row-label">
+                    <h1>3. Preview your embed</h1>
+                </div>
+                <div className="row-interaction">
+                    <div id="card-embed"></div>
+                </div>
+            </div>
         )
     }
 
