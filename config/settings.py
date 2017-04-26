d8f6b33b910daeab9f9bc3e3d406757168664884
Tyler Fisher <tyler@tylerjfisher.com>
2017-04-26 14:12:34 -0400

    allowed hosts

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = secrets['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0', 'localhost', app_config.STAGING_SERVERS[0], app_config.PRODUCTION_SERVERS[0]]
 
 
 # Application definition
