973c667326fbfbbcb9544a040c47fb6a804b9c9e
Tyler Fisher <tyler@tylerjfisher.com>
2017-05-02 13:26:22 -0400

    max length on titles/subtitles, last updated field

--- core/models.py
+++ core/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-
 class Category(models.Model):
     category_name = models.CharField(max_length=40)
 
@@ -14,14 +13,15 @@ class Category(models.Model):
 class Card(models.Model):
     published = models.BooleanField(default=False)
     copyedited = models.BooleanField(default=False)
-    title = models.CharField(max_length=100)
-    subtitle = models.CharField(max_length=140, null=True, blank=True)
-    lede = models.CharField(max_length=140, null=True)
+    title = models.CharField(max_length=75)
+    subtitle = models.CharField(max_length=75, null=True, blank=True)
+    lede = models.CharField(max_length=75, null=True)
     body = models.TextField()
     image = models.ImageField(null=True, blank=True, upload_to='img/')
     image_credit = models.CharField(max_length=140, null=True, blank=True)
     production_notes = models.TextField(null=True, blank=True)
     category = models.ForeignKey(Category)
+    last_updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
