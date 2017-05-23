c23df3827fcda9ba09f86f3b16459074695a5626
Tyler Fisher <tyler@tylerjfisher.com>
2017-05-23 10:20:39 -0400

    screenshots fab function

--- fabfile/data.py
+++ fabfile/data.py
@@ -1,7 +1,17 @@
 import app_config
+import requests
 
 from fabric.api import hide, local, settings, shell_env, task
+from fabric.contrib import django
 from fabric.state import env
+from slugify import slugify
+
+
+# django setup
+django.settings_module('config.settings')
+import django
+django.setup()
+from core.models import Card
 
 @task
 def create_db():
@@ -21,3 +31,17 @@ def create_db():
 
         if env.get('settings'):
             execute('servers.start_service', 'uwsgi')
+
+@task
+def screenshots():
+    LAMBDA_URL = 'https://zy98z1okb0.execute-api.us-east-1.amazonaws.com/dev/chrome'
+
+    for card in Card.objects.all():
+        print('screenshoting {0}'.format(card.title))
+
+        payload = {
+            'id': card.id,
+            'slug': slugify(card.title)
+        }
+
+        r = requests.get(LAMBDA_URL, params=payload)
